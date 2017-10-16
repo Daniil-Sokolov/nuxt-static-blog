085a77fb9fa6d3834ef62d1166ad5d33c252994e
jonniek <kayhty@windowslive.com>
2017-10-16 10:58:18 +0900

    rename again

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Jonniek homepage',
+    title: 'Homepage | Jonniek',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
